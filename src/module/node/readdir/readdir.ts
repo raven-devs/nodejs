0e0c5aeceb5ff5b1b49753bce9eb01fbdffd09ab
spetushkou <sergey.petushkou@gmail.com>
2023-08-29 20:55:22 +0200

    feat: :rocket: #NOREF-0

---
+++ src/module/node/readdir/readdir.ts
@@ -0,0 +1,18 @@
+import { readdir } from 'fs/promises';
+
+/**
+ * npx ts-node src/module/node/readdir/readdir.ts
+ */
+
+const getDirectories = async (source: string) => {
+  return (await readdir(source, { withFileTypes: true }))
+    .filter((dirent) => dirent.isDirectory())
+    .map((dirent) => dirent.name);
+};
+
+const run = async () => {
+  const directories = await getDirectories(process.cwd());
+  console.log({ directories });
+};
+
+run();
